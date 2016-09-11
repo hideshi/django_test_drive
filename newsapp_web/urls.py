46ed1cffb08f0f7d50d950bef6362bd5af97cc82
Hideshi Ogoshi <hideshi.ogoshi@gmail.com>
2016-09-11 08:16:14 +0800

    Renamed project

--- newsapp_web/urls.py
+++ newsapp_web/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken import views as rest_views
-from newsapp.urls import router as newsapp
+from newsapp.urls import router
 from newsapp import views
 
 urlpatterns = [
@@ -26,7 +26,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     url(r'^api-auth/', rest_views.obtain_auth_token),
-    url(r'^api/', include(newsapp.urls)),
+    url(r'^api/', include(router.urls)),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
